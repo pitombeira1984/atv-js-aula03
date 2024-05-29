befa645c12dc9ab5a3a35e7504b1fa812dc1ade3
pitombeira1984 <168240874+pitombeira1984@users.noreply.github.com>
2024-05-29 16:11:09 -0300

    Add files via upload

---
+++ ATV03/dom.js
@@ -0,0 +1,10 @@
+   
+//--Atividade 03--//   
+   var n = Number(prompt("Digite a 1º Número:"))
+   var soma = 0
+   var soma = soma + n    
+   while(n >= 1){    
+       n = Number(prompt("Digite a numero seguinte:"))
+       soma = soma + n
+       console.log(`Soma = ${(soma)}`)
+  }
