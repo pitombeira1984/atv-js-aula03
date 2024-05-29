de161a6af459c5e9d41b45f614c631270570fb72
pitombeira1984 <168240874+pitombeira1984@users.noreply.github.com>
2024-05-29 16:12:39 -0300

    Add files via upload

---
+++ ATV06/dom.js
@@ -0,0 +1,19 @@
+//--Atividade 06 - Soma os numeros de 1 a 50 de forma crescente--//
+// let contador = Number(prompt("Digite o numero entre 1 e 50:"))
+// let soma = 0
+// while(contador <= 50){      
+//     soma = soma + contador
+//     contador++    
+// }
+// console.log(`Soma = ${soma}`)
+// console.log("Soma os numeros de 1 a 50 de forma crescente")
+
+//--Atividade 06 - Soma os numeros de 1 a 50 de forma decrescente--//
+let contador = Number(prompt("Digite o numero entre 1 e 50:"))
+let soma = 0
+while(contador > 0){      
+    soma = soma + contador
+    contador--    
+}
+console.log(`Soma = ${soma}`)
+console.log("Soma os numeros de 1 a 50 de forma decrescente")
